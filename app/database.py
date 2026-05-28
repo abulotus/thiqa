54cd10bef3d774107afca023f78cd34d8209c427
Wesam <abulotus@hotmail.com>
2026-05-29 00:07:38 +0100

    test1

--- app/database.py
+++ app/database.py
@@ -1,7 +1,11 @@
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("postgresql://postgres:WbJuliJYuqrrdrbADgZOLiZboKdhQeaC@postgres-vdl5.railway.internal:5432/railway")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is missing")
 
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace(
@@ -11,3 +15,11 @@ if DATABASE_URL.startswith("postgres://"):
     )
 
 engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
