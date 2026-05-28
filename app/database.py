55bb3f2e0bffee1f2676b0918337f4e449d6d812
Wesam <abulotus@hotmail.com>
2026-05-29 00:28:40 +0100

    Update DATABASE_URL to use environment variable

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("postgresql://postgres:WbJuliJYuqrrdrbADgZOLiZboKdhQeaC@postgres-vdl5.railway.internal:5432/railway")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is missing")
